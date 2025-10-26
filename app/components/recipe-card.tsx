5725b09a52aa469896232b9f8ad04e1ee4d07f09
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2025-10-26 22:17:52 +0100

    Add custom pictures to recipe cards

--- app/components/recipe-card.tsx
+++ app/components/recipe-card.tsx
@@ -1,6 +1,7 @@
 import { Types } from "mongoose";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export interface IRecipeCard {
 	name: string;
@@ -9,21 +10,24 @@ export interface IRecipeCard {
 }
 
 function RecipeCard({ name, content, _id }: IRecipeCard) {
+	const [imageError, setImageError] = useState(false);
+
 	return (
 		<Link
 			href={"/recipes/" + _id.toString()}
 			className="w-72 h-96 rounded-xl overflow-hidden shrink-0"
 		>
-			<div className="relative w-full h-48">
-				{_id ? (
+			<div className="relative w-full h-48 bg-(--primary-border) flex items-center justify-center">
+				{_id && !imageError ? (
 					<Image
 						className="object-cover"
-						src="/recipe.jpg"
+						src={"/images/" + _id.toString() + ".jpg"}
 						alt={"Recipe Picture"}
 						fill
+						onError={() => setImageError(true)}
 					/>
 				) : (
-					<div></div>
+					<div className="text-8xl text-(--primary)">?</div>
 				)}
 			</div>
 			<div className="p-4 bg-(--primary) rounded h-full">
